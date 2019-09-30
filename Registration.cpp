2c6f00ffa756797ac2d02c433f89e0a1966a2c0d
Bharath <bharathrajeevnair@gmail.com>
2019-09-30 15:03:12 +0930

    formatting

--- Registration.cpp
+++ Registration.cpp
@@ -56,12 +56,12 @@ string currentDateTime() {
     return ab;
 }
 
-string convertToString(char* a, int size) 
-{ 
-    int i; 
-    string s = ""; 
-    for (i = 0; i < size; i++) { 
-        s = s + a[i]; 
-    } 
-    return s; 
+string convertToString(char* a, int size)
+{
+    int i;
+    string s = "";
+    for (i = 0; i < size; i++) {
+        s = s + a[i];
+    }
+    return s;
 }
