ccbed8cff85b1d8194d33ccc66968c5c2d77269a
zakariya0 <44619084+zakariya0@users.noreply.github.com>
2019-10-06 19:16:02 +1030

    Update Faculty.cpp

--- Faculty.cpp
+++ Faculty.cpp
@@ -1,9 +1,53 @@
 #include <iostream>
+using namespace std;
 #include <string>
 #include "Faculty.h"
-using namespace std;
+#include "Degree.h"
 
-Faculty::Faculty()
+Faculty::Faculty():Degree()                          
+{
+	facultysize = 0;
+	facultyname = "ECMS";
+	degnum = 0;
+}	
+
+Faculty::Faculty(int max_size, string name)
+{
+	facultysize = max_size;
+	facultyname = name;
+}
+
+int Faculty::get_current_number_of_Degrees()
+{
+	return degnum;
+}
+	
+string Faculty::get_name()
+{
+	return facultyname;
+}
+	
+bool Faculty::add_degree(Degree new_degree)
+{
+	if(degnum < facultysize)
+		{
+			roster[degnum] = new_degree;
+			
+			degnum++;
+			return true;
+		}
+	else
+	{
+		return false;
+	}
+}
+
+Degree * Faculty::get_roster()
+{
+		return roster;
+}
+	
+Faculty::~Faculty()
 {
 
 }
