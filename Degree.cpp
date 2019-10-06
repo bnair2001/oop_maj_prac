ccd4d88f46d47b1f717ca4087d2646b41aef0da4
zakariya0 <44619084+zakariya0@users.noreply.github.com>
2019-10-06 18:53:00 +1030

    Create Degree.cpp

---
+++ Degree.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+#include <string>
+#include "Degree.h"
+
+Degree::Degree()              // a default constructor 
+{
+	dnum = 0;
+	names = "Bachelor of Science in Computer science";
+}
+
+Degree::Degree(int dNumber, string dName)    // a constructor that takes the degree number and degree name
+{
+	names = dName;
+	degnum = dNumber;
+}
+
+string Degree::get_name()        // returns the name of the Degree
+{
+	return names;
+}
+
+int Degree::get_degree_number()  // returns the Degree's Degree number
+{
+	return degnum;
+}
+
+Degree::~Degree()                //A default destructor
+{
+
+}
