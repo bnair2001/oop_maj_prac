eb8a4f8f35a8db01f2732ee53f5f406764303a67
zakariya0 <44619084+zakariya0@users.noreply.github.com>
2019-10-06 18:46:27 +1030

    Create Degree.h

---
+++ Degree.h
@@ -0,0 +1,17 @@
+#ifndef DEGREE_H
+#define DEGREE_H
+using namespace std;
+#include <string>
+
+class Degree
+{
+public:
+	Degree();                  // a default constructor 
+	Degree(int dNumber, std::string dName);    // a constructor that takes the degree number and degree name
+	string get_name();        // returns the name of the degree
+	int get_degree_number();  // returns the degree's number
+	~Degree();                //A default destructor
+	int degnum;
+	string names;
+};
+#endif
