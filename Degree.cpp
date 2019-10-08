4f5d6320688b51d3ca3bc7dff94533b86f58de33
Chanukar <55957493+Chanukar@users.noreply.github.com>
2019-10-09 06:08:37 +1030

    Update Degree.cpp

--- Degree.cpp
+++ Degree.cpp
@@ -1,31 +1,9 @@
-#include <iostream>
-using namespace std;
-#include <string>
 #include "Degree.h"
 
-Degree::Degree()              // a default constructor
-{
-	degnum = 0;
-	names = "default_value";
-}
+Degree::Degree() : Faculty(), degree_name("Not Initialised") {}
 
-Degree::Degree(int dNumber, string dName)    // a constructor that takes the degree number and degree name
-{
-	names = dName;
-	degnum = dNumber;
-}
 
-string Degree::get_name()        // returns the name of the Degree
-{
-	return names;
-}
-
-int Degree::get_degree_number()  // returns the Degree's Degree number
-{
-	return degnum;
-}
-
-Degree::~Degree()                //A default destructor
-{
-
-}
+Degree::Degree(string faculty_name, string degree_name) : Faculty(faculty_name), degree_name(degree_name) {}
+        
+        
+string Degree::get_degree() {return degree_name;}
